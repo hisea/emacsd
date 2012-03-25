bfecabfbb4eab85a0c5da704e00745e61d06c603
Hisea <zyinghai@gmail.com>
2012-03-24 21:44:08 -0400

    init

---
+++ init.el
@@ -0,0 +1,52 @@
+(push "/usr/local/bin" exec-path)
+
+(setq mac-command-modifier 'meta)
+;;(setq mac-control-modifier 'alt)
+;;(setq mac-option-modifier 'control)
+
+(setq make-backup-files nil)
+(setq auto-save-default nil)
+(setq-default tab-width 2)
+(setq-default indent-tabs-mode nil)
+(setq inhibit-startup-message t)
+(setq visible-bell t)
+
+(fset 'yes-or-no-p 'y-or-n-p)
+
+(delete-selection-mode t)
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(blink-cursor-mode t)
+(show-paren-mode t)
+(column-number-mode t)
+(set-fringe-style -1)
+(tooltip-mode -1)
+
+(set-frame-font "Menlo-15")
+(set-fontset-font
+    (frame-parameter nil 'font)
+    'han
+    (font-spec :family "Hiragino Sans GB" ))
+
+(require 'package)
+(setq package-archives (cons '("tromey" . "http://tromey.com/elpa/") package-archives))
+(package-initialize)
+
+
+;; ido
+(require 'ido)
+(ido-mode t);; ;; rinari
+
+(require 'rinari)
+
+(require 'sass-mode)
+
+;; Textmate mode
+(require 'textmate)
+(textmate-mode)
+
+(require 'haml-mode)
+
+(add-to-list 'load-path
+              "~/.emacs.d/plugins/yasnippet")
+(require 'yasnippet-bundle)
