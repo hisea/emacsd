95d1a323a0168e738dea19b3c316ea186680867b
Hisea <zyinghai@gmail.com>
2012-03-25 13:09:08 -0400

    using git instead of elpa for some mode/plugin

--- init.el
+++ init.el
@@ -1,5 +1,6 @@
 (push "/usr/local/bin" exec-path)
 
+
 ;; Mac keyboard modifier key
 ;;(setq mac-command-modifier 'meta)
 ;;(setq mac-option-modifier 'alt)
@@ -23,16 +24,29 @@
 (set-fringe-style -1)
 (tooltip-mode -1)
 
+;; Font
+
 (set-frame-font "Menlo-15")
 (set-fontset-font
     (frame-parameter nil 'font)
     'han
     (font-spec :family "Hiragino Sans GB" ))
 
+;; Theme
+(add-to-list 'custom-theme-load-path 
+             "~/.emacs.d/themes/emacs-color-theme-solarized")
+(add-to-list 'custom-theme-load-path 
+             "~/.emacs.d/themes/zenburn-emacs")
+
+;;(load-theme 'solarized-light t)
+;;(load-theme 'solarized-dark t) ;; solarized dark theme 
+(load-theme 'tango' t)
+;;(load-theme 'zenburn' t)
+
+
 (require 'package)
 (setq package-archives (cons '("tromey" . "http://tromey.com/elpa/") package-archives))
 (package-initialize)
-
 
 ;; start of plugins
 (require 'ido)
@@ -43,14 +57,19 @@
 (require 'rinari)
 
 ;; Textmate mode
+(add-to-list 'load-path "~/.emacs.d/plugins/textmate.el")
 (require 'textmate)
 (textmate-mode)
 
+(add-to-list 'load-path "~/.emacs.d/plugins/rvm.el")
 (require 'rvm)
 (rvm-use-default) ;; use rvm's default ruby for the current Emacs session
 
-(require 'sass-mode)
+(add-to-list 'load-path "~/.emacs.d/plugins/haml-mode")
 (require 'haml-mode)
+(add-to-list 'load-path "~/.emacs.d/plugins/sass-mode")
+(require 'sass-mode)
+
 
 (add-to-list 'load-path "~/.emacs.d/plugins/mode-compile")
 (add-to-list 'load-path "~/.emacs.d/plugins/rspec-mode")
@@ -73,35 +92,14 @@
 (add-to-list 'auto-mode-alist '("Cakefile" . coffee-mode))
 
 ;;rails
-(defun ruby-mode-hook ()
-  (autoload 'ruby-mode "ruby-mode" nil t)
-  (add-to-list 'auto-mode-alist '("Capfile" . ruby-mode))
-  (add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))
-  (add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
-  (add-to-list 'auto-mode-alist '("\\.rake\\'" . ruby-mode))
-  (add-to-list 'auto-mode-alist '("\\.rb\\'" . ruby-mode))
-  (add-to-list 'auto-mode-alist '("\\.ru\\'" . ruby-mode))
-  (add-hook 'ruby-mode-hook '(lambda ()
-                               (setq ruby-deep-arglist t)
-                               (setq ruby-deep-indent-paren nil)
-                               (setq c-tab-always-indent nil)
-                               (require 'inf-ruby)
-                               (require 'ruby-compilation))))
+(add-to-list 'auto-mode-alist '("Capfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.rake\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.rb\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.ru\\'" . ruby-mode))
 
 ;; Yaml
-(defun yaml-mode-hook ()
-  (autoload 'yaml-mode "yaml-mode" nil t)
-  (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
-  (add-to-list 'auto-mode-alist '("\\.yaml$" . yaml-mode)))
-
-;; Theme
-(add-to-list 'custom-theme-load-path 
-             "~/.emacs.d/themes/emacs-color-theme-solarized")
-(add-to-list 'custom-theme-load-path 
-             "~/.emacs.d/themes/zenburn-emacs")
-
-;;(load-theme 'solarized-light t)
-;;(load-theme 'solarized-dark t) ;; solarized dark theme 
-(load-theme 'tango' t)
-;;(load-theme 'zenburn' t)
+(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+(add-to-list 'auto-mode-alist '("\\.yaml$" . yaml-mode))
 
