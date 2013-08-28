961774f291b9d55bb597e0991ed39e459fbeca6a
Yinghai Zhao <yinghai.zhao@zoocasa.com>
2013-08-28 13:14:11 -0400

    update mode config

--- config/mode_config.el
+++ config/mode_config.el
@@ -8,8 +8,6 @@
 (setq auto-mode-alist (cons '("Gemfile*" . ruby-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '("Capfile" . ruby-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '("\\.erb\\'" . rhtml-mode) auto-mode-alist))
-(add-hook 'ruby-mode-hook
-          (lambda () (local-set-key (kbd "RET") 'reindent-then-newline-and-indent))) ;; hitting enter will indent.
 
 (add-hook 'haml-mode-hook 'highlight-indentation-mode)
 (add-hook 'slim-mode-hook 'highlight-indentation-mode)
