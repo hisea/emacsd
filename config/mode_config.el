9fae5fe8468a183ecf5504cb2563a040df2f4db3
Yinghai Zhao <zyinghai@gmail.com>
2015-01-25 23:31:18 -0500

    Add org mode config

--- config/mode_config.el
+++ config/mode_config.el
@@ -54,3 +54,17 @@
       '(("php"    . "\\.phtml\\'")
         ("php"  . "\\.php\\."))
       )
+
+;; Org Mode
+(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
+
+(setq org-todo-keywords
+      '((sequence "TODOi(t)" "STARTED(s)" "WAIT(w)" "|" "DONE(d)" "CANCELLED(c)")))
+
+(setq org-tag-alist
+      '(("work" . ?w)
+        ("home" . ?h)
+        ("phone" . ?p)))
